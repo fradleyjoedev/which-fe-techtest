5f43710d2e5571b4b6eef1a47e0b7aeb53cefeab
Joe Fradley <jfradley@sortable.co.uk>
2020-04-07 15:14:20 +0100

    Added base styles to the numbers and containers

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ import { totalNumberDisplayed, mainBodyContainerId } from './config.json';
 //This function appends the numbers 
 const appendNumbers = () => {
   const numberContainer = document.createElement('div');
+  numberContainer.classList.add("container");
 
   // Starting loop at 1
   for (let i = 1; i <= totalNumberDisplayed; i++) {
     const number = document.createElement('div');
 
     number.setAttribute('numberId', i);
+    number.classList.add("container__element");
     number.innerHTML = i;
     numberContainer.appendChild(number);
   }
