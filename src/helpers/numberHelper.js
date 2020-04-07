999cd25c91ed88a5144eea8c101bc53add49b527
Joe Fradley <jfradley@sortable.co.uk>
2020-04-07 17:49:10 +0100

    Removing active numbers if another number is clicked

--- src/helpers/numberHelper.js
+++ src/helpers/numberHelper.js
@@ -1,25 +1,43 @@
 import { totalNumberDisplayed, mainBodyContainerId } from '../config/config.json';
 
 export default class NumberHelper {
+  constructor() {
+    // A type of state to determine if multiples are being 
+    // displayed
+    this.activeMultiples = false;  
+  }
+
   // Find each multiple of the number selected
-  findMultipleNumbers(element) {
-    if (element.target && element.target.attributes) {
-      const number = element.target.attributes.numberid.value;
-  
-      for (let i = 1; i <= totalNumberDisplayed; i++) {
-        // divide by the number
-        if (i % number === 0) { 
-          const element = document.querySelector(`[numberid='${i}']`);
-  
-          if(element) {
-            element.classList.add("container__element--active");
+  selectMultipleNumbers(element) {
+    if(!this.activeMultiples) {
+      if (element.target && element.target.attributes) {
+        const number = element.target.attributes.numberid.value;
+    
+        for (let i = 1; i <= totalNumberDisplayed; i++) {
+          // divide by the number
+          if (i % number === 0) { 
+            const element = document.querySelector(`[numberid='${i}']`);
+    
+            if(element) {
+              element.classList.add("container__element--active");
+            }
+
+            this.activeMultiples = true;
           }
         }
+      } else {
+        // Proper error handling should be here.
+        console.log('Cannot find attribute for numberid');
       }
     } else {
-      // Proper error handling should be here.
-      console.log('Cannot find attribute for numberid');
+      this.removeMultipleNumbers();
     }
+  }
+
+  // Remove all active elements selected
+  removeMultipleNumbers() {
+    document.querySelectorAll('.container__element--active').forEach(el => el.classList.remove('container__element--active'));
+    this.activeMultiples = false;
   }
 
   // Append numbers based on the amount defined in the config
@@ -33,7 +51,7 @@ export default class NumberHelper {
   
       number.setAttribute('numberId', i);
       number.classList.add("container__element");
-      number.onclick = this.findMultipleNumbers;
+      number.onclick = this.selectMultipleNumbers.bind(this);
       number.innerHTML = i;
       numberContainer.appendChild(number);
     }
